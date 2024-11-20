11b3e8f7aa5b7b97b04441f5d10fe426f361ad85
Max <maxleal.sg@gmail.com>
2024-11-20 17:55:21 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-   return <h1>Uma longa viagem começa com um unico passo. <br></br>(Lao-Tsé) </h1>
+   return <h1>Uma longa viagem começa com um unico passo. <br></br> Lao-Tsé </h1>
 
 }
 
